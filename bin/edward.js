20effe525bc3922b3272b31ac982bf3ff9e29120
coderaiser <mnemonic.enemy@gmail.com>
2015-01-29 10:52:21 -0500

    feature(package) add try-catch

--- bin/edward.js
+++ bin/edward.js
@@ -3,7 +3,10 @@
 (function() {
     'use strict';
     
-    var rendy           = require('rendy'),
+    var error,
+        fs              = require('fs'),
+        rendy           = require('rendy'),
+        tryCatch        = require('try-catch'),
         args            = process.argv.slice(2),
         arg             = args[0];
         
@@ -11,20 +14,28 @@
         version();
     else if (!arg || /^(-h|--help)$/.test(arg))
         help();
-    else
-        main();
-       
-    function main() {
-        var DIR         = __dirname + '/../assets',
+    else {
+        error = tryCatch(function() {
+            fs.statSync(arg);
+        });
+        
+        if (error)
+            console.error(error.message);
+        else
+            main(arg);
+    }
+    
+    function main(name) {
+        var socket,
+            DIR         = __dirname + '/../assets/',
             edward      = require('../'),
             http        = require('http'),
             express     = require('express'),
             io          = require('socket.io'),
             restafary   = require('restafary'),
             
-            server      = http.createServer(),
-            app         = express(server),
-            socket      = io.listen(server),
+            app         = express(),
+            server      = http.createServer(app),
             
             port        =   process.env.PORT            ||  /* c9           */
                             process.env.app_port        ||  /* nodester     */
@@ -33,23 +44,49 @@
             ip          =   process.env.IP              ||  /* c9           */
                             '0.0.0.0',
             
-            str = rendy('Edward not ready to read "{{ name }}"', {
-                name: arg
-            });
+            index       = fs.readFileSync(DIR + 'index.html', 'utf8');
         
         app .use(express.static(DIR))
             .use(restafary())
             .use(edward({
                 minify: false
-            }));
+            }))
+            .get('/file', function(req, res) {
+                readFile(name, index, function(error, data) {
+                    if (error)
+                        res .status(404)
+                            .send(error.message);
+                    else
+                        res.send({
+                            name: name,
+                            data: data
+                        });
+                });
+            });
         
+        server.listen(port, ip),
+        
+        socket      = io.listen(server),
         edward.listen(socket);
-        app.listen(port, ip);
         
         console.log('url: http://' + ip + ':' + port);
-        console.log(str);
     }
-       
+    
+    function readFile(name, index, callback) {
+        fs.readFile(name, 'utf8', function(error, value) {
+            var data;
+            
+            if (!error)
+                data = rendy(index, {
+                    name: name,
+                    value: value
+                });
+            
+            callback(error, data);
+        });
+    }
+    
+    
     function version() {
         console.log('v' + info().version);
     }
