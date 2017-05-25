9b8505c17641c83d143ce81aa6801df237f28c15
coderaiser <coderaiser@cloudcmd.io>
2017-05-25 14:18:51 +0300

    chore(story) es2015-ify

--- client/story.js
+++ client/story.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* global restafary */
+
 module.exports = Story;
 
 function Story() {
@@ -7,11 +9,11 @@ function Story() {
         return new Story();
 }
 
-Story.prototype.checkHash              = function(name, callback) {
-    this.loadHash(name, function(error, loadHash) {
-        var nameHash    = name + '-hash',
-            storeHash   = localStorage.getItem(nameHash),
-            equal       = loadHash === storeHash;
+Story.prototype.checkHash = function(name, callback) {
+    this.loadHash(name, (error, loadHash) => {
+        const nameHash = name + '-hash';
+        const storeHash = localStorage.getItem(nameHash);
+        const equal = loadHash === storeHash;
         
         callback(error, equal);
     });
@@ -19,40 +21,40 @@ Story.prototype.checkHash              = function(name, callback) {
     return this;
 };
 
-Story.prototype.loadHash               = function(name, callback) {
-    var query       = '?hash';
+Story.prototype.loadHash = function(name, callback) {
+    const query = '?hash';
     
     restafary.read(name + query, callback);
     
     return this;
 };
 
-Story.prototype.setData                = function(name, data) {
-    var nameData    = name + '-data';
+Story.prototype.setData = function(name, data) {
+    const nameData = name + '-data';
     
     localStorage.setItem(nameData, data);
     
     return this;
 };
 
-Story.prototype.setHash                = function(name, hash) {
-    var nameHash    = name + '-hash';
+Story.prototype.setHash = function(name, hash) {
+    const nameHash = name + '-hash';
     
     localStorage.setItem(nameHash, hash);
     
     return this;
 };
 
-Story.prototype.getData                = function(name) {
-    var nameData    = name + '-data',
-        data        = localStorage.getItem(nameData);
+Story.prototype.getData = function(name) {
+    const nameData = name + '-data';
+    const data = localStorage.getItem(nameData);
     
     return data || '';
 };
 
-Story.prototype.getHash                = function(name) {
-    var item    = name + '-hash',
-        data    = localStorage.getItem(item);
+Story.prototype.getHash = function(name) {
+    const item = name + '-hash';
+    const data = localStorage.getItem(item);
     
     return data || '';
 };
